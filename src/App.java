7d5da3da233c12fe6977c1b59ce2023dc54b9adc
AnKaSc <anna.schuback@hotmail.se>
2018-09-04 15:05:21 +0200

    Add a new class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+
 public class App {
     public static void main(String [] args) {
-        System.out.println("Test!");
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MainFrame frame = new MainFrame();
+                frame.setSize(500, 400);
+                frame.setLocationRelativeTo(null);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
     }
 }
